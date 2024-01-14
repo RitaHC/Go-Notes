307a448498f66d0211edc7978f2475dffa60e88f
RitaHC <ritahameerchauhan@ritas-air.mynetworksettings.com>
2024-01-14 11:27:36 -0500

    Initial commit

---
+++ variables.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Types of Variables
+	// Bool
+
+	// string
+
+	// INTEGERES:   int int8 int16 int32 int64
+	// + or - Whole numbers
+	// UNSIGNED INTEGERES: uint uint8 uint16 uint32 uint64 uintptr
+	// ONLY + integers
+
+	// byte // alias for uint8
+
+	// rune // alias for int32
+	// represents a Unicode code point
+
+	// float32 float64 : Fractional numbers
+
+	// complex64 complex128 : Imaginary numbers
+
+	// USAGE
+	// 1) Byte might be used a lot while marshalling a JSON object to be sent across a network connection.
+	// 2) Rune a one character in a string
+
+	// DECLARATION
+	var number int = 4
+	// var hasPermission bool
+	// var username string
+
+	// PRINT
+	fmt.Println("Welcome Back!")
+	fmt.Println(number)
+
+	// HOW TO RUN A GO CODE
+	// make the folder/file inside the go path
+	//
+}
